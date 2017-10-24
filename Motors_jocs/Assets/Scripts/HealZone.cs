744a87bd3f46831ce94983b0ed95eee75ed41508
Sens <recasenx@gmail.com>
2017-10-24 04:17:21 +0200

    Heal Zone Mechanic
    Cura una certa quantitat de vida quan el jugador entra dins una zona. Un cop recuperada aquesta quantitat de vida, ja no cura més.

---
+++ Motors_jocs/Assets/Scripts/HealZone.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealZone : MonoBehaviour {
+
+    GameObject player;
+    PlayerHealth playerHealth;
+
+    public float timeBetweenHeal = 1.5f;
+    public int healAmmount = 5;
+    public int maxHealTimes = 5;
+    public int HealTimes = 0;
+    float timer;
+    bool playerInRange = false;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag("FPSController");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+    }
+
+	void OnTriggerEnter (Collider other)
+    {
+        Debug.Log("object entered the trigger");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("object staying in the trigger");
+        playerInRange = true;
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("object out the trigger");
+        playerInRange = false;
+
+    }
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > timeBetweenHeal && HealTimes < maxHealTimes && playerInRange)
+        {
+            Debug.Log("Heal()");
+            Heal();
+        }
+    }
+
+    public void Heal()
+    {
+        //reset the timer
+        timer = 0f;
+
+        //if the player have not the full life
+        if(playerHealth.currentHealth < 100)
+        {
+            //heal the player
+            playerHealth.HealPlayer(healAmmount);
+
+            HealTimes++;
+        }
+    }
+}
---
+++ Motors_jocs/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 50;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    void Awake ()
+    {
+        //set the initial health of the player
+        currentHealth = startingHealth;
+        healthSlider.value = currentHealth;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+
+    }
+
+    public void HealPlayer (int amount)
+    {
+        currentHealth += amount;
+        healthSlider.value = currentHealth;
+    }
+}
