989fbc35eb4f664b2d4c1c7e9206da13a29d2e3f
Sens <recasenx@gmail.com>
2017-11-13 16:37:37 +0100

    All things mixed and meteor
    All things are mixed, and the player shoots a meteor.

---
+++ Motors_jocs/Assets/BulletScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    // Use this for initialization
+    public float expiryTime = 0f;
+
+    void Start () {
+        Destroy(gameObject, expiryTime);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Motors_jocs/Assets/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    // Use this for initialization
+    public float moveForce = 0f;
+    private Rigidbody rbody;
+    public GameObject bullet;
+    public Transform gun;
+    public float shootRate = 0f;
+    public float shootForce = 0f;
+    private float shootRateTimeStamp = 0f;
+
+	void Start () {
+        rbody = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal") * moveForce;
+        float v = Input.GetAxisRaw("Vertical") * moveForce;
+
+        //rbody.velocity = new Vector3(h, v, 0);
+        
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (Time.time > shootRateTimeStamp)
+            {
+                GameObject go = (GameObject)Instantiate(bullet, gun.position, gun.rotation);
+                go.GetComponent<Rigidbody>().AddForce(gun.forward * shootForce);
+                shootRateTimeStamp = Time.time + shootRate;
+            }
+        }
+    }
+}
