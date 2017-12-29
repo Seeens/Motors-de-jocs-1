f633e762895b8dbedb3570ed284e68670435ccab
Sir Meeseeks <ginardfalgueras@gmail.com>
2017-12-29 17:17:27 +0100

    Static projector

---
+++ Motors_jocs/Assets/_Scripts/Player_Projector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Projector : MonoBehaviour
+{
+
+	private Vector3 originPosition;
+	private Vector3 directionCast;
+
+	private RaycastHit _hit;
+	private Projector _projector;
+
+	void Start ()
+	{
+		_projector = GameObject.FindWithTag("Projector").GetComponent<Projector>();
+		originPosition = GameObject.GetComponent<Transform>().position;
+		directionCast = GetComponent<Transform>().forward;
+	}
+
+	void Update ()
+	{
+		Physics.Raycast(originPosition, directionCast, out _hit);
+
+	}
+
+}
